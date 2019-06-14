4036f37e3bba0767e88ff27dc362af18b64b571a
Szabolcs Surányi <szabolcs.suranyi@shadesoft.hu>
2019-06-14 13:18:07 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+
+let bw;
+
+function createWindow() {
+	bw = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true
+		},
+		icon: __dirname + '/icon.png'
+	});
+
+	bw.loadFile('index.html');
+
+	bw.on('closed', () => {
+		bw = null;
+	});
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+	app.quit();
+});
+
+app.on('activate', () => {
+	if(bw === null) {
+		createWindow();
+	}
+});
